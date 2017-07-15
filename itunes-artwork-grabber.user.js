d7504e573bca4379565b05ec087272e48425071d
sparanoid <t@sparanoid.com>
2017-07-15 22:08:15 +0800

    chore: release 1.0.7, bump again for reband issue

--- itunes-artwork-grabber.user.js
+++ itunes-artwork-grabber.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Artwork Grabber by Sparanoid
 // @namespace    https://sparanoid.com/work/itunes-artwork-grabber/
-// @version      1.0.6
+// @version      1.0.7
 // @description  Yet another artwork grabber
 // @author       Sparanoid
 // @include      *://itunes.apple.com/*/*
